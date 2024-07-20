3adcf70e9c892b3a04693071dd1150af4870b06c
Jake Moy <apixelatedpug@gmail.com>
2024-07-20 13:59:38 -0700

    :sparkles: Add slowdown time to movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,31 +3,59 @@ using System;
 
 public partial class Player : Sprite2D
 {
+	Vector2 velocity;
+
+	[Export]
+	public float SlowdownMult = 0.95f;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		var velocity = Vector2.Zero;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
+	// NOTE: I'm not sure as of yet, but this current movement system (I assume) 
+	// makes those with higher framerates faster. Look into using deltatime.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero;
+		bool horizKeyDown = false, vertKeyDown = false;
 
 		// Handling movement input
 		if (Input.IsActionPressed("ui_left")) {
-			velocity.X -= 1;
+			velocity.X = -1;
+			horizKeyDown = true;
 		}
 		if (Input.IsActionPressed("ui_right")) {
-			velocity.X += 1;
+			velocity.X = 1;
+			horizKeyDown = true;
 		}
 		if (Input.IsActionPressed("ui_up")) {
-			velocity.Y -= 1;
+			velocity.Y = -1;
+			vertKeyDown = true;
 		}
 		if (Input.IsActionPressed("ui_down")) {
-			velocity.Y += 1;
+			velocity.Y = 1;
+			vertKeyDown = true;
 		}
 
+		// Slow down the velocity until below a threshold to stop
+
+		// Ensuring skid-stop doesn't occur along other axis if we're still moving
+		if (!vertKeyDown && !horizKeyDown) {
+			velocity.Y *= Math.Abs(velocity.Y) < 0.2 ? 0 : SlowdownMult;
+		} else if (!vertKeyDown && horizKeyDown) {
+			velocity.Y = 0;
+		}
+
+		if (!horizKeyDown && !vertKeyDown) {
+			velocity.X *= Math.Abs(velocity.X) < 0.2 ? 0 : SlowdownMult;
+		} else if (!horizKeyDown && vertKeyDown) {
+			velocity.Y = 0;
+		}
+
+		// Apply velocity to object position
 		Position += velocity;
 	}
 }
