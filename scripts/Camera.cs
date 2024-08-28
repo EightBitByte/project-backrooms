9358c45980a00aff6242928291ff5cb79d2a0e35
Jake Moy <apixelatedpug@gmail.com>
2024-08-27 23:15:53 -0700

    :boom: Implement view camera

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -3,7 +3,12 @@ using System;
 
 public partial class Camera : Camera2D
 {
+	[Export]
+	float SafeArea = 300;
+
 	CharacterBody2D tracking;
+	double timeElapsed;
+	Vector2 mousePos;
 
 	public override void _Ready()
 	{
@@ -14,6 +19,22 @@ public partial class Camera : Camera2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position = tracking.Position;
+		timeElapsed += delta;
+
+		mousePos = GetGlobalMousePosition();
+		timeElapsed = 0;
+
+		Vector2 center = tracking.Position;
+
+
+		Vector2 calcPosition = center - (center - mousePos)/2;
+
+		// If the distance from the center is greater than the safe area, clamp to safe area
+		if (calcPosition.DistanceTo(center) > SafeArea)
+			;
+
+
+		Position = calcPosition;
+
 	}
 }
